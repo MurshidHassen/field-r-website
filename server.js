dd56e3e76c6b546f54a81d96b94f777bfbbb735c
Murshid Hassen <mh.murshidco.uk@outlook.com>
2020-07-12 19:52:12 +0530

    changes done

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ const creds = require('./config');
 //     }
 // });
 
+const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -118,9 +122,8 @@ router.post('/investment', (req, res, next) => {
 })
 
 
-const path = require('path')
-const PORT = process.env.PORT || 5000
-const app = express()
+
+
 app.use(express.json())
 app.use('/', router)
 
